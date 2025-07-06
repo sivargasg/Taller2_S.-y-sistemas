a59b51fb5cd3829feabb79e6cc1b92d7005f9260
sivargasg <sivargasg@unal.edu.co>
2025-07-06 10:54:58 -0500

    Update modulator.m

---
+++ Modulator/modulator.m
@@ -0,0 +1,19 @@
+function modulated = modulator(signal, fc, fs)
+    % fc     : frecuencia de la portadora
+    % fs     : frecuencia de muestreo
+
+    bw = 15E3;
+    filtered = lowpass(signal, bw, fs);
+
+    t = (0:length(filtered)-1) / fs;
+    osc = cos(2*pi*fc*t).';
+
+    modulated = filtered .* osc;
+
+    figure;
+    plot(t, modulated);
+    title(['Señal modulada a ', num2str(fc/1000), ' kHz']);
+    xlabel('Tiempo (s)');
+    ylabel('Amplitud');
+
+end
